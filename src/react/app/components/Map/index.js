441d0cc377451be913a9be14687d695fc25d0a8a
crow <crow@crow.crow>
2021-04-20 22:24:43 +0100

    Created a new way for map update and mechanic working

--- src/react/app/components/Map/index.js
+++ src/react/app/components/Map/index.js
@@ -19,49 +19,42 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
     width: "80%",
-  },
-  row1: {
-    marginBottom: 100,
-  },
-  row2: {
-    marginBottom: 200,
-  },
-  row3: {
-    marginBottom: 300,
-  },
-  row4: {
-    marginBottom: 400,
-  },
-  row5: {
-    marginBottom: 500,
-  },
-  row6: {
-    marginBottom: 600,
-  },
+  }
 });
 
-const Tiles = [
-  "Combate",
-  "Combate Élite",
-  "Tesoro",
-  "Tienda",
-  "Hoguera",
-  "Misión",
-  "Sala misteriosa",
-  "Boss",
-];
-const RamdomTiles = [
-  "Combate",
-  "Combate Elíte",
-  "Tesoro",
-  "Tienda",
-  "Sala misteriosa",
-  "Hoguera",
-];
+const tileStates = ["visitable", "actual", "visited", "disabled", "unknown"];
 
-const initialState = {
-  position: 1,
-};
+export class TileModel{
+  id;
+  state;
+  bgColor;
+
+  constructor(id, state) {
+    this.id = id;
+    this.state = state;
+    this.bgColor = this.asignColor();
+  }
+
+  asignColor = () => {
+    if(tileStates[this.state] === tileStates[0]) {
+      return "green";
+    } else if(tileStates[this.state] === tileStates[1]) {
+      return "yellow";
+    } else if(tileStates[this.state] === tileStates[2]) {
+      return "red";
+    } else if(tileStates[this.state] === tileStates[3]) {
+      return "grey";
+    } else if(tileStates[this.state] === tileStates[4]) {
+      return "blue";
+    }
+  }
+
+  changeState = (state) => {
+    this.state = state;
+    this.asignColor();
+    console.log(this.state);
+  }
+}
 
 //Component declaration
 export default class index extends Component {
@@ -70,130 +63,40 @@ export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ...initialState,
+      /* tileMap: [
+        {
+          id: 0,
+          state: tileStates[0]
+        },
+        {
+          id: 1,
+          state: tileStates[4]
+        }
+      ] */
+      tileMap: new TileModel(0, 0)
     };
   }
 
-  actualPosition = () => {
-      return this.state.position;
-  }
-
-  ramdomEvent = () => {
-    let probability = Math.floor(Math.random() * RamdomTiles.length);
-    return RamdomTiles[probability];
-  }
-
-  advancePosition = () => {
-      this.state.position++;
+  updateMap = () => {
+    this.setState({
+      tileMap: new TileModel(this.state.tileMap.id, this.state.tileMap.state) 
+    })
   }
 
   render() {
     //Marks the functions and variables that the needed is going to use
     const {} = this.props;
-    const { position } = this.state;
+    const {tileMap} = this.state;
     return (
       <View style={styles.container}>
-        <View style={[styles.row, styles.row1]}>
-          <Tile
-            identifier={1}
-            tileType={Tiles[0]}
-            position={this.actualPosition}
-            levelFloor={1}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={2}
-            tileType={Tiles[0]}
-            position={this.actualPosition}
-            levelFloor={1}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={3}
-            tileType={Tiles[0]}
-            position={this.actualPosition}
-            levelFloor={1}
-            advancePosition={this.advancePosition}
-          />
-        </View>
-
-        <View style={[styles.row, styles.row2]}>
-          <Tile
-            identifier={4}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={2}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={5}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={2}
-            advancePosition={this.advancePosition}
-          />
-        </View>
-
-        <View style={[styles.row, styles.row3]}>
-          <Tile
-            identifier={6}
-            tileType={Tiles[4]}
-            position={this.actualPosition}
-            levelFloor={3}
-            advancePosition={this.advancePosition}
-          />
-        </View>
-
-        <View style={[styles.row, styles.row4]}>
-          <Tile
-            identifier={7}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={4}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={8}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={4}
-            advancePosition={this.advancePosition}
-          />
-        </View>
-
-        <View style={[styles.row, styles.row5]}>
-          <Tile
-            identifier={9}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={5}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={10}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={5}
-            advancePosition={this.advancePosition}
-          />
-          <Tile
-            identifier={11}
-            tileType={this.ramdomEvent()}
-            position={this.actualPosition}
-            levelFloor={5}
-            advancePosition={this.advancePosition}
-          />
-        </View>
-
-        <View style={[styles.row, styles.row6]}>
-          <Tile
-            identifier={12}
-            tileType={Tiles[7]}
-            position={this.actualPosition}
-            levelFloor={6}
-            advancePosition={this.advancePosition}
-          />
-        </View>
+        <Tile
+          color={tileMap}
+          updateMap={this.updateMap}
+        />
+        <Tile
+          color={tileMap}
+          updateMap={this.updateMap}
+        />
       </View>
     );
   }
--- src/react/app/components/Tiles/index.js
+++ src/react/app/components/Tiles/index.js
@@ -1,31 +1,16 @@
 //Imports
 import React, { Component } from "react";
 import {
-  View,
-  Text,
   StyleSheet,
-  Modal,
-  TouchableOpacity,
-  TextInput,
   Pressable,
 } from "react-native";
 
 //Stylesheet
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
   tile: {
     width: 50,
     height: 50,
   },
-  tileUnselected: {
-    backgroundColor: "pink",
-  },
-  tileSelected: {
-    backgroundColor: "red",
-  }
 });
 
 //Component declaration
@@ -34,43 +19,23 @@ export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      backgroundColor: "pink"
     };
-  }
-
-  _changeStyle() {
-    this.setState({
-      backgroundColor: this.props.position() == this.props.levelFloor ? "red" : "pink"
-    })
   }
 
   render() {
     //Marks the functions and variables that the needed is going to use
 
-    const {
-      identifier,
-      tileType,
-      position,
-      levelFloor,
-      advancePosition,
-    } = this.props;
+    const {color, updateMap} = this.props;
     const {} = this.state;
 
     return (
-      <Pressable
-        className={styles.tileUnselected}
-        style={[styles.tile, {backgroundColor: this.state.backgroundColor}]}
+      <Pressable 
+        style={[styles.tile, styles.tileUnselected, {backgroundColor: color.bgColor}]}
         onPress={() => {
-          console.log("piso actual: " + position());
-          if (position() == levelFloor) {
-            this._changeStyle();
-            console.log("sala: " + identifier + ", tipo: " + tileType);
-            advancePosition();
-            console.log("se avanza al piso: " + position());
-          } else {
-            console.log("se está intentando acceder a una sala del piso " + levelFloor)
+            color.changeState(1);
+            updateMap();
           }
-        }}
+        }
       />
     );
   }
