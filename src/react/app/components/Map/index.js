d26ff502215523bfe0212df00520e609c5f58576
crow <crow@crow.crow>
2021-04-27 19:55:52 +0100

    Finally working map

--- src/react/app/components/Map/index.js
+++ src/react/app/components/Map/index.js
@@ -1,9 +1,6 @@
 //Imports
 import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import Tile from "../Tiles";
 
@@ -12,48 +9,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    flexDirection: "column-reverse"
   },
   row: {
-    bottom: 0,
-    position: "absolute",
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "80%",
-  }
+  },
 });
 
 const tileStates = ["visitable", "actual", "visited", "disabled", "unknown"];
 
-export class TileModel{
+export class TileModel {
   id;
   state;
   bgColor;
+  level;
 
-  constructor(id, state) {
+  constructor(id, state, level) {
     this.id = id;
     this.state = state;
+    this.level = level;
     this.bgColor = this.asignColor();
   }
 
   asignColor = () => {
-    if(tileStates[this.state] === tileStates[0]) {
+    if (tileStates[this.state] === tileStates[0]) {
       return "green";
-    } else if(tileStates[this.state] === tileStates[1]) {
+    } else if (tileStates[this.state] === tileStates[1]) {
       return "yellow";
-    } else if(tileStates[this.state] === tileStates[2]) {
+    } else if (tileStates[this.state] === tileStates[2]) {
       return "red";
-    } else if(tileStates[this.state] === tileStates[3]) {
+    } else if (tileStates[this.state] === tileStates[3]) {
       return "grey";
-    } else if(tileStates[this.state] === tileStates[4]) {
+    } else if (tileStates[this.state] === tileStates[4]) {
       return "blue";
     }
-  }
+  };
 
   changeState = (state) => {
     this.state = state;
     this.asignColor();
-    console.log(this.state);
   }
+
 }
 
 //Component declaration
@@ -63,40 +60,69 @@ export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      /* tileMap: [
-        {
-          id: 0,
-          state: tileStates[0]
-        },
-        {
-          id: 1,
-          state: tileStates[4]
-        }
-      ] */
-      tileMap: new TileModel(0, 0)
+      tileMap: [
+        [new TileModel(0, 0, 0), new TileModel(1, 0, 0)],
+        [new TileModel(2, 4, 1)],
+        [new TileModel(3, 4, 2), new TileModel(4, 4, 2)],
+        [new TileModel(5, 4, 3), new TileModel(6, 4, 3), new TileModel(7, 4, 3)],
+        [new TileModel(8, 4, 4), new TileModel(9, 4, 4)],
+        [new TileModel(10, 4, 5)]
+      ],
     };
   }
 
-  updateMap = () => {
+  updateMap = (i) => {
+    const tempMap = [];
+    this.state.tileMap.forEach((row) => {
+      const tempArray = [];
+      row.forEach((tile) => {
+        tempArray.push(new TileModel(tile.id, tile.state, tile.level));
+      });
+      tempMap.push(tempArray);
+    });
     this.setState({
-      tileMap: new TileModel(this.state.tileMap.id, this.state.tileMap.state) 
-    })
+      tileMap: tempMap,
+    });
+  };
+
+  globalChangeState = (tile) => {
+    console.log(tile);
+    if(tile.state == 0){
+      if(tile.level != 0) {
+        this.state.tileMap[tile.level-1].forEach(tile2 => {
+          if(tile2.state == 1) {
+            tile2.changeState(2);
+          }
+        });
+      }
+      this.state.tileMap[tile.level].forEach(tile2 => {
+        if(tile2.id == tile.id) {
+          tile2.changeState(1);
+        } else {
+          tile2.changeState(3);
+        }
+      });
+      if(this.state.tileMap.length > tile.level + 1){
+        this.state.tileMap[tile.level+1].forEach(tile2 => {
+            tile2.changeState(0);
+        });
+      }
+    }
   }
 
   render() {
     //Marks the functions and variables that the needed is going to use
     const {} = this.props;
-    const {tileMap} = this.state;
+    const { tileMap } = this.state;
     return (
       <View style={styles.container}>
-        <Tile
-          color={tileMap}
-          updateMap={this.updateMap}
-        />
-        <Tile
-          color={tileMap}
-          updateMap={this.updateMap}
-        />
+        {tileMap.map((row, key) => (
+          <View key={key} style={styles.row}>
+            {row.map((tile) => (
+              <Tile key={tile.id} tile={tile} updateMap={this.updateMap} updateTile={this.globalChangeState}/>
+            ))}
+          </View>
+        ))}
       </View>
     );
   }
--- src/react/app/components/Tiles/index.js
+++ src/react/app/components/Tiles/index.js
@@ -10,6 +10,8 @@ const styles = StyleSheet.create({
   tile: {
     width: 50,
     height: 50,
+    margin: 25,
+    borderRadius: 50
   },
 });
 
@@ -25,14 +27,14 @@ export default class index extends Component {
   render() {
     //Marks the functions and variables that the needed is going to use
 
-    const {color, updateMap} = this.props;
+    const {tile, updateMap, updateTile} = this.props;
     const {} = this.state;
 
     return (
       <Pressable 
-        style={[styles.tile, styles.tileUnselected, {backgroundColor: color.bgColor}]}
+        style={[styles.tile, styles.tileUnselected, {backgroundColor: tile.asignColor()}]}
         onPress={() => {
-            color.changeState(1);
+            updateTile(tile);
             updateMap();
           }
         }
